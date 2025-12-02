518ad9d52dc357c6d8bbdef05bd381c984806013
zhangchuan <zhangchuan@samsino.com.cn>
2025-12-02 11:54:17 +0800

    too

--- dist/utils/mqtt.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function mqttStart({ username, password, customTopics, devUrl }: {
-    username?: string;
-    password?: string;
-    customTopics?: string[];
-    devUrl?: string;
-}): void;
-export declare function mqttStop(): void;
